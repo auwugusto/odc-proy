9a64865913740b046952fc0e0fc53c2750f4ee3f
auwugusto <acopell@mi.unc.edu.ar>
2024-06-04 18:18:19 -0300

    add new makefile flags

--- start.s
+++ start.s
@@ -1,45 +1,45 @@
-	.text
-	.org 0x80000
+.text
+.org 0x80000
 
-	// Setup Frame Buffer
-	.equ SCREEN_WIDTH, 		640
-	.equ SCREEN_HEIGH, 		480
-	.equ BITS_PER_PIXEL, 	32
+// Setup Frame Buffer
+.equ SCREEN_WIDTH, 640
+.equ SCREEN_HEIGH, 480
+.equ BITS_PER_PIXEL, 32
 
-	.equ MAIL_BASE,  0xB880 // Mailbox Base Address
-	.equ MAIL_WRITE, 0x20 // Mailbox Write Register
-	.equ MAIL_TAGS,  0x8    // Mailbox Channel 8: Tags (ARM to VC)
-	.equ PERIPHERAL_BASE, 0x3F000000 // Peripheral Base Address
+.equ MAIL_BASE, 0xB880 // Mailbox Base Address
+.equ MAIL_WRITE, 0x20 // Mailbox Write Register
+.equ MAIL_TAGS, 0x8 // Mailbox Channel 8: Tags (ARM to VC)
+.equ PERIPHERAL_BASE, 0x3F000000 // Peripheral Base Address
 
-	.equ Get_Allocate_Buffer,      0x00040001 // Frame Buffer: Allocate Buffer (Response: Frame Buffer Base Address In Bytes, Frame Buffer Size In Bytes)
+.equ Get_Allocate_Buffer, 0x00040001 // Frame Buffer: Allocate Buffer (Response: Frame Buffer Base Address In Bytes, Frame Buffer Size In Bytes)
+.equ Set_Physical_Display, 0x00048003 // Frame Buffer: Set Physical (Display) Width/Height (Response: Width In Pixels, Height In Pixels)
+.equ Set_Virtual_Buffer, 0x00048004 // Frame Buffer: Set Virtual (Buffer) Width/Height (Response: Width In Pixels, Height In Pixels)
+.equ Set_Depth, 0x00048005 // Frame Buffer: Set Depth (Response: Bits Per Pixel)
+.equ Set_Virtual_Offset, 0x00048009 // Frame Buffer: Set Virtual Offset (Response: X In Pixels, Y In Pixels)
+.equ Set_Palette, 0x0004800B // Frame Buffer: Set Palette (Response: RGBA Palette Values (Index 0 To 255))
+.equ Set_Pixel_Order, 0x00048006 // Frame Buffer: Set Pixel Order
+.equ Get_Pixel_pitch, 0x00048006 // Frame Buffer: Set Pixel Order
 
-	.equ Set_Physical_Display, 0x00048003 // Frame Buffer: Set Physical (Display) Width/Height (Response: Width In Pixels, Height In Pixels)
-	.equ Set_Virtual_Buffer,   0x00048004 // Frame Buffer: Set Virtual (Buffer) Width/Height (Response: Width In Pixels, Height In Pixels)
-	.equ Set_Depth,            0x00048005 // Frame Buffer: Set Depth (Response: Bits Per Pixel)
-	.equ Set_Virtual_Offset,   0x00048009 // Frame Buffer: Set Virtual Offset (Response: X In Pixels, Y In Pixels)
-	.equ Set_Palette,          0x0004800B // Frame Buffer: Set Palette (Response: RGBA Palette Values (Index 0 To 255))
-	.equ Set_Pixel_Order,	   0x00048006 // Frame Buffer: Set Pixel Order
-	.equ Get_Pixel_pitch,	   0x00048006 // Frame Buffer: Set Pixel Order
+// UART data
+.equ Set_Power, 0x00028001 // UART: Set Power
+.equ Set_ClkRate, 0x00038002 // UART: Set Clock Rate
 
-	// UART data
-	.equ Set_Power,    0x00028001 // UART: Set Power
-	.equ Set_ClkRate,  0x00038002 // UART: Set Clock Rate
+.equ MMIO_BASE_ALTA, 0x3f20
 
-	.equ MMIO_BASE_ALTA,    0x3f20
-	
-	.equ GPFSEL0,			0x0000
-	.equ GPFSEL1,			0x0004
-	.equ GPPUD,				0x0094
-	.equ GPPUDCLK0,			0x0098
+.equ GPFSEL0, 0x0000
+.equ GPFSEL1, 0x0004
+.equ GPPUD, 0x0094
+.equ GPPUDCLK0, 0x0098
 
-	.equ UART0_DR,    0x1000
-	.equ UART0_FR,    0x1018
-	.equ UART0_IBRD,  0x1024
-	.equ UART0_FBRD,  0x1028
-	.equ UART0_LCRH,  0x102C
-	.equ UART0_CR,    0x1030
-	.equ UART0_IMSC,  0x1038
-	.equ UART0_ICR,   0x1044
+.equ UART0_DR, 0x1000
+.equ UART0_FR, 0x1018
+.equ UART0_IBRD, 0x1024
+.equ UART0_FBRD, 0x1028
+.equ UART0_LCRH, 0x102C
+.equ UART0_CR, 0x1030
+.equ UART0_IMSC, 0x1038
+.equ UART0_ICR, 0x1044
+
 // Return CPU ID (0..3) Of The CPU Executed On
 
 .section ".text.boot"
